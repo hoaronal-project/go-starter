955b217ef42be60579b52bd30defab2a40dd012b
hoaronal-project <50139926+hoaronal-project@users.noreply.github.com>
2022-02-06 09:05:37 +0700

    Initialize mr/go-swagger@v0.27.0

---
+++ internal/api/handlers/common/get_swagger_test.go
@@ -0,0 +1,17 @@
+package common_test
+
+import (
+	"net/http"
+	"testing"
+
+	"allaboutapps.dev/aw/go-starter/internal/api"
+	"allaboutapps.dev/aw/go-starter/internal/test"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSwaggerYAMLRetrieval(t *testing.T) {
+	test.WithTestServer(t, func(s *api.Server) {
+		res := test.PerformRequest(t, s, "GET", "/swagger.yml", nil, nil)
+		require.Equal(t, http.StatusOK, res.Result().StatusCode)
+	})
+}
---
+++ internal/util/slice_test.go
@@ -0,0 +1,16 @@
+package util_test
+
+import (
+	"testing"
+
+	"allaboutapps.dev/aw/go-starter/internal/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContainsString(t *testing.T) {
+	test := []string{"a", "b", "d"}
+	assert.True(t, util.ContainsString(test, "a"))
+	assert.True(t, util.ContainsString(test, "b"))
+	assert.False(t, util.ContainsString(test, "c"))
+	assert.True(t, util.ContainsString(test, "d"))
+}
